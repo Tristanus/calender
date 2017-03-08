ba0c987d0d780ac204cb0f5d69a11198870fe2fd
DESKTOP-S0MLQRE\Tristan <info@tristandejager.com>
2017-03-08 22:37:32 +0100

    new pages. made db connection. started addDate page

---
+++ head.php
@@ -0,0 +1,3 @@
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="style.css">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Calender</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-</head>
-<body>
-	<h1>Calender</h1>	
-</body>
+	<head>
+		<title>Calender</title>
+		<?php include "head.php"; ?>
+	</head>
+
+	<body>
+		<h1>Calender</h1>	
+	</body>
 </html>
